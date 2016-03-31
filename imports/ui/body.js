6f7fd4e2d7b6f861b5614dbcd50ab399f5d1117b
Josue Basurto <josuebasurto@gmail.com>
2016-03-30 23:50:18 -0700

    Storing tasks in a collection https://www.meteor.com/tutorials/blaze/collections

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,11 @@
 import { Template } from 'meteor/templating';
  
+import { Tasks } from '../api/tasks.js';
+ 
 import './body.html';
  
 Template.body.helpers({
-  tasks: [
-    { text: 'This is task number 1' },
-    { text: 'This is task number 2' },
-    { text: 'This is task number 3' },
-  ],
+  tasks() {
+    return Tasks.find({});
+  },
 });
